05d325bed0dfce8028d69a957ce73844aa2c98bf
jewelrydango <jewelrydango@gmail.com>
2021-07-01 00:05:31 +0900

    bootstrapの適用

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,11 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.webjars:jquery:3.6.0")
+	implementation("org.webjars:bootstrap:5.0.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/com/jewelrydango/smilesmilemovie/app/CommentController.kt
@@ -0,0 +1,16 @@
+package com.jewelrydango.smilesmilemovie.app
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+
+
+@Controller
+class CommentController {
+
+    @RequestMapping("/")
+    fun index(): String? {
+        return "index"
+    }
+}
