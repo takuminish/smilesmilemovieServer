86d1897c1fe8b3d3846d64ab9c1e22e3f86556c9
jewelrydango <jewelrydango@gmail.com>
2021-06-30 23:24:35 +0900

    first commit

---
+++ src/main/kotlin/com/jewelrydango/smilesmilemovie/SmileSmileMovieApplication.kt
@@ -0,0 +1,11 @@
+package com.jewelrydango.smilesmilemovie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmileSmileMovieApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmileSmileMovieApplication>(*args)
+}
---
+++ src/test/kotlin/com/jewelrydango/smilesmilemovie/SmileSmileMovieApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jewelrydango.smilesmilemovie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmileSmileMovieApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
