edba24e3f360ebab35f86c50aef4f0a5b03ddfa5
jewelrydango <jewelrydango@gmail.com>
2021-07-13 23:53:33 +0900

    ajaxによるコメント送信処理の作成

---
+++ src/main/kotlin/com/jewelrydango/smilesmilemovie/domain/model/SmileComment.kt
@@ -0,0 +1,12 @@
+package com.jewelrydango.smilesmilemovie.domain.model
+
+class SmileComment {
+    var comment: String;
+
+    var color: String;
+
+    constructor(comment: String, color: String) {
+        this.comment = comment;
+        this.color = color;
+    }
+}
